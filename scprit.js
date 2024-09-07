fcab2e06faa87ac85f6c56ae11400e23e4df3a17
ashish605 <ashishvs605@gmail.com>
2024-09-07 15:58:04 +0530

    Initial commit

---
+++ scprit.js
@@ -0,0 +1,24 @@
+// let a="harry"
+// console.log(a.length)
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+// console.log(a[4])
+
+// let b="ashish"
+// let c="kushal"
+// console.log(`his name is ${b} and his friend is ${c}`) //backtik
+
+// console.log("har \nry")
+
+let g="ashish"
+let f="sageena"
+
+// console.log(g.length)
+// console.log(g.toUpperCase())
+// console.log(g.toLowerCase())
+// console.log(g.replace("ash","sag"))
+
+console.log(g.concat(f))
+console.log(g.concat("weds",f))
